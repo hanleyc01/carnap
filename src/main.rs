65d5ec0b7bc46ed785aedd55b878fd5c9bba04a7
Connor Hanley <connordhanley@gmail.com>
2022-06-18 19:50:15 -0500

    added source and filter
    added the source and filter parts of speech production, i worked out a graphical representation of data :33

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,83 @@ struct Filter {
     round: Cavity,
 }
 
+impl Filter {
+
+    fn new(pharynx: Cavity, oral: Cavity, round: Cavity) -> Self {
+       Self { pharynx, oral, round } 
+    }
+
+    fn formants(&self, source_wave: Source) -> Vec<SimpleWave> {
+        let mut result1: Vec<SimpleWave> = Vec::new();
+        
+        for harmonic in source_wave.complex_wave {
+            let f1 = harmonic.clone();
+            result1.push(self.pharynx.amp_and_damp(f1));
+        }
+        
+        let mut result2: Vec<SimpleWave> = Vec::new();
+        for harmonic in result1 {
+            let f2 = harmonic.clone();
+            result2.push(self.oral.amp_and_damp(f2));
+        }
+
+        let mut result3: Vec<SimpleWave> = Vec::new();
+        for harmonic in result2 {
+            let f3 = harmonic.clone();
+            result3.push(self.round.amp_and_damp(f3));
+        }
+        
+        result3
+        
+    }
+}
+
 /// The distinct filters within the overall Filter (e.g., pharyngeal cavity,
 /// oral cavity, and lip roundedness)
 struct Cavity {
-    resonance_freq: Vec<f32>,
+    resonance_freq: f64,
+    phase: Option<f64>,
+}
+
+impl Cavity {
+    
+    /// Constructs a cavity given area, volume, and length of the cavity
+    fn new(area: f64, volume: f64, length: f64, phase: f64) -> Self {
+        let v_sound = 343f64;
+        let v_div_2pi = v_sound / 2f64 * std::f64::consts::PI;
+        let sqrt_a_div_vl = (area / volume * length).sqrt();
+        let f_res = v_div_2pi * sqrt_a_div_vl;
+        Self { 
+            resonance_freq: f_res,
+            phase: {
+                if phase == 0.0 {
+                    None
+                } else {
+                    Some(phase)
+                }
+            }
+        }
+    }
+
+    /// Amplify and dampen harmonics
+    fn amp_and_damp(&self, mut harmonic: SimpleWave) -> SimpleWave {
+        match self.phase {
+            Some(p) => {
+                harmonic.amplitude = 2. * harmonic.amplitude * (harmonic.phase - p / 2.).cos();
+                harmonic
+            },
+            None => {
+                harmonic.amplitude = 2. * harmonic.amplitude * (harmonic.phase / 2.).cos();
+                harmonic
+            },
+        }
+    }
+
 }
 
 /// Source in our source-filter theory of speech;
 /// This is the primary abstraction for the vocal folds vibration.
+#[derive(Clone)]
 struct Source {
    complex_wave: Vec<SimpleWave>, 
 }
@@ -25,6 +94,7 @@ struct Source {
 // +++ Physical
 
 /// Abstraction representing a simple sinusoidal wave
+#[derive(Clone)]
 struct SimpleWave {
     amplitude: f64,
     period: f64, 
@@ -53,7 +123,7 @@ impl SimpleWave {
             amplitude,
             period: 1. / frequency,
             frequency,
-            angular_frequency: 2. *  std::f64::consts::PI * frequency,
+            angular_frequency: 2. * std::f64::consts::PI * frequency,
             phase,
         }
     }
@@ -70,12 +140,28 @@ impl SimpleWave {
         }
     }
 
+    fn sine_wave(amplitude: f64, phase: f64) -> Self {
+       use std::f64::consts::PI;
+       let period = 2.0 * PI;
+       let frequency = 1.0 / period;
+       let angular_frequency = 2.0 * PI * frequency;
+       Self {
+            amplitude,
+            period,
+            frequency,
+            angular_frequency,
+            phase
+       }
+    }
+
     /// Calculate displacement of height over some time period, range of [init_time, fin_time)
     fn displace_y(&self, init_time: f64, fin_time: f64, step: f64) -> Vec<f64> {
+        use std::f64::consts::PI;
+
         let mut displacement: Vec<f64> = Vec::new();
         let range = get_range(init_time, fin_time, step);
         for t in range {
-            let body = self.angular_frequency * t + self.phase;
+            let body = 2.0 * PI * self.frequency * t + self.phase;
             displacement.push(self.amplitude * body.sin());
         }
 
@@ -99,16 +185,14 @@ fn get_range(init_time: f64, fin_time: f64, step: f64) -> Vec<f64> {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Test simple wave
-    let test_wave: SimpleWave = SimpleWave::default();
-    let init_time = 0.4;
-    let fin_time = 1.;
-    let step = 0.03;
+    let test_wave: SimpleWave = SimpleWave::sine_wave(2., 1.);
+    let init_time = 0.0;
+    let fin_time = 15.;
+    let step = 0.001;
     let test_displace = test_wave.displace_y(init_time, fin_time, step);
     let range = get_range(init_time, fin_time, step);
-
-    println!("{:#?} {:#?}", &test_displace, &range);
         
-    println!("Range generated!");
+    println!("wave generated!");
 
     // The chart
     let root = BitMapBackend::new("1.png", (640, 480)).into_drawing_area();
@@ -118,11 +202,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .margin(5u32)
         .x_label_area_size(50u32)
         .y_label_area_size(50u32)
-        .build_cartesian_2d(0.4..1.0, -0.2..0.2)?;
+        .build_cartesian_2d(init_time..fin_time, -5.000..5.3000)?;
   
     chart.configure_mesh().draw()?;
 
-    println!("range generated");
+    println!("wave drawn");
 
     chart
         .draw_series(LineSeries::new(std::iter::zip(range, test_displace), &RED))?
@@ -133,7 +217,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .border_style(&BLACK)
         .draw()?;
 
-    println!("Chart drawn!");
+    println!("chart drawn");
 
     root.present()?;
 
